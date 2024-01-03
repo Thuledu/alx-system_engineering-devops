b87dcf8dc8b8639fa894beecb6abbc46aa298f13
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-01-03 10:39:17 -0800

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,16 +1,13 @@
 #!/usr/bin/env ruby
-# Sample input data representing text messages transactions
-input_data = [
-  "From: +1234567890 To: +9876543210 Sent: HELLO",
-  "From: Alice To: Bob Sent: LOL",
-  "From: +447123456789 To: +447987654321 Sent: INFO"
-]
+# Read the log file and process each line
+File.open('log_file.txt', 'r') do |file|
+  file.each_line do |line|
+    if line.include?("Receive SMS") || line.include?("Sent SMS")
+      sender = line[/from:(\S+)/, 1]
+      receiver = line[/to:(\S+)/, 1]
+      flags = line[/flags:([\d:-]+)/, 1]
 
-# Process the input data and output the required information
-input_data.each do |transaction|
-  sender = transaction[/From: (\S+)/, 1]
-  receiver = transaction[/To: (\S+)/, 1]
-  flags = transaction.scan(/Sent: (\S+)/).join(',')
-
-  puts "#{sender},#{receiver},#{flags}"
+      puts "#{sender},#{receiver},#{flags}"
+    end
+  end
 end
