1ba017214a42566023ddac21c719b762dbbcc88a
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-01-03 11:24:14 -0800

    Regular expressions

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
-# Accept the argument from the command line
-argument = ARGV[0]
-
-# Regular expression to match the word "School"
-regex = /School/
-
-# Check if the argument matches the regular expression and output the result
-if argument.match?(regex)
-  puts argument
-else
-  puts ""
-end
+# Regular expression that accepts one argument and pass it to a regular expression matching method.
+puts ARGV[0].scan(/hb?tn/).join
--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,9 +1,3 @@
 #!/usr/bin/env ruby
-# Accept the argument from the command line
-argument = ARGV[0]
-
-# Regular expression to match the word "School"
-regex = /School/
-
 # Check if the argument matches the regular expression and output the result
-puts argument.match?(regex) ? argument : ""
+puts ARGV[0].scan(/hbt+n/).join
