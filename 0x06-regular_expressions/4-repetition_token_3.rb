f7fd52e91c4cac5356b7f147445b34f50705ceb9
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-01-03 11:08:46 -0800

    Repetition Token #3

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,19 +1,3 @@
 #!/usr/bin/env ruby
-# Get the input argument
-log_entry = ARGV[0]
-
-# Define the regular expression pattern
-regex_pattern = /from:(\S+)\s.*to:(\S+)\s.*flags:([\d:-]+)/
-
-# Match the regular expression pattern with the log entry
-matches = log_entry.match(regex_pattern)
-
-# Output the matched groups if there is a match
-if matches
-  sender = matches[1]
-  receiver = matches[2]
-  flags = matches[3]
-  puts "#{sender},#{receiver},#{flags}"
-else
-  puts "No match found"
-end
+# This Ruby script accepts one argument and passes it to a regular expression matching method.
+puts ARGV[0].scan(/hbt*n/).join
