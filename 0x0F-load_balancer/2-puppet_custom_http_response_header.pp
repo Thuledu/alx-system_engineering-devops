c26f80f0abc7ac4bc0a5b83a4b448121f81182ce
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-03-04 17:33:19 -0800

    0x0F. Load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # Custom HTTP header response in server
 
+# update ubuntu server
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+
 # Install Nginx package
 package { 'nginx':
   ensure => installed,
+  provider => 'apt',
 }
 
 # Define custom HTTP header response
@@ -14,6 +22,8 @@ $custom_header = "add_header X-Served-By ${hostname};"
 file { '/etc/nginx/server.headers':
   ensure  => present,
   content => "location / { ${custom_header} }",
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
   notify  => Service['nginx'],
 }
 
